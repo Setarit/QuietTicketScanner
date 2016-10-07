0ecb6e35a9c94f7050e6467911d60ef4f735b3fd
JValck <jasper_de_valck@hotmail.com>
2016-10-07 18:19:34 +0200

    Select camera

--- app/src/main/java/com/setarit/quietticketscanner/ScanController.java
+++ app/src/main/java/com/setarit/quietticketscanner/ScanController.java
@@ -1,14 +1,42 @@
 package com.setarit.quietticketscanner;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.camera2.*;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.setarit.quietticketscanner.view.listeners.CameraSourceChangeListener;
 
 public class ScanController extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_controller);
         setTitle(R.string.scan);
+
+        try {
+            getAllCameras();
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void getAllCameras() throws CameraAccessException {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String[] cameraIds = manager.getCameraIdList();
+        Spinner cameraList = (Spinner) findViewById(R.id.camera_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cameraIds);
+        if(cameraList != null) {
+            cameraList.setAdapter(adapter);
+        }
+        cameraList.setOnItemClickListener(new CameraSourceChangeListener(this, adapter));
+        return ;
     }
 }
