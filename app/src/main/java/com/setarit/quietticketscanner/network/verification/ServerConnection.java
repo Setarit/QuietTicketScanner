54cef8e9d908bf90db9471034ac1236601e86fe7
JValck <jasper_de_valck@hotmail.com>
2017-10-18 16:39:23 +0200

    Cleanup

--- app/src/main/java/com/setarit/quietticketscanner/network/verification/ServerConnection.java
+++
@@ -1,9 +0,0 @@
-package com.setarit.quietticketscanner.network.verification;
-
-/**
- * Created by Setarit on 01/10/2016.
- * Contains the connection to the server
- */
-
-public class ServerConnection {
-}
